f8faea7911325d8916b259938384194544188810
paubernabe <pbernaco7@alumnes.ub.edu>
2020-10-20 20:38:05 +0200

    fixes in books columns and icons added

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -4,25 +4,37 @@
   <b-navbar toggleable="lg" type="dark" variant="info">
    <b-navbar-brand href="#">NavBar</b-navbar-brand>
    <b-navbar-nav class="ml-auto"> <!-- Right aligned -->
-     <b-button variant="danger">Log In</b-button>
+   <ul id="menu-main-nav" class="navbar-nav nav-fill w-100">
+     <li id="menu-item-42" class="nav-item menu-item menu-item-type-post_type menu-item-object-page menu-item-home current-menu-item page_item page-item-6 current_page_item menu-item-42"><a href="XXX" class="nav-link">     <b-icon icon="basket" font-scale="2.5"></b-icon>
+</a></li>
+     <li id="menu-item-963" class="nav-item menu-item menu-item-type-post_type menu-item-object-page menu-item-963"><a href="XXX" class="nav-link">     <b-button variant="danger">Log In</b-button>
+</a></li>
+     </ul>
    </b-navbar-nav>
   </b-navbar>
  </div>
 
 <!-- container for static pics -->
-
+<br>
+<br>
 
 <b-container>
- <img :src="'https://placehold.it/800x300/?text=' + picturestock" alt="">
+ <img :src="'https://placehold.it/1100x300/?text=' + picturestock" alt="">
 </b-container>
 
 <!-- Carousel -->
+<style>
+.container{
+padding : 1px;
+
+}
+</style>
 <div class="container">
    <h3> Best sellers </h3>
-   <div class="row">
-     <div class="col-lg-4 col-md-6 mb-4" v-for="i in 3" :key="i">
+   <b-row>
+     <b-col  v-for="i in 6" :key="i">
        <br>
-       <img :src="'https://placehold.it/200x200/?text=' + i + '-' + i" alt="">
+       <img :src="'https://placehold.it/140x218/?text=' + i + '-' + i" alt="">
        <h6>Llibre</h6>
        <div v-for="j in 1" :key="j">
           <h5>AUTOR</h5>
@@ -30,10 +42,9 @@
        <h6>Valoració</h6>
        <h6>PREU</h6>
        <b-button variant="danger">Add to cart</b-button>
-     </div>
-   </div>
- </div>
-  </div>
+   </b-col>
+   </b-row>
+</div>
 </template>
 
 <script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,12 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import BootstrapVue from 'bootstrap-vue'
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
 import '@/../bootstrap/css/bootstrap.css'
 import Vue from 'vue'
 import App from './App'
 import router from './router'
 Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
