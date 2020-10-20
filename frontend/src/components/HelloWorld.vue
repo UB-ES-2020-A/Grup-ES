579bc3b435dc3034726464aca92e50ad4c8a0991
paubernabe <pbernaco7@alumnes.ub.edu>
2020-10-20 16:21:46 +0200

    Pagina principal en procés

--- frontend/src/components/HelloWorld.vue
+++ frontend/src/components/HelloWorld.vue
@@ -1,113 +1,45 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
+<div id="app">
+ <div>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+   <b-navbar-brand href="#">NavBar</b-navbar-brand>
+   <b-navbar-nav class="ml-auto"> <!-- Right aligned -->
+     <b-button variant="danger">Log In</b-button>
+   </b-navbar-nav>
+  </b-navbar>
+ </div>
+
+<!-- container for static pics -->
+<b-container>
+ <img :src="'https://placehold.it/800x300/?text=' + picturestock" alt="">
+</b-container>
+
+<!-- Carousel -->
+<div class="container">
+   <h3> Best sellers </h3>
+   <div class="row">
+     <div class="col-lg-4 col-md-6 mb-4" v-for="i in 3" :key="i">
+       <br>
+       <img :src="'https://placehold.it/200x200/?text=' + i + '-' + i" alt="">
+       <h6>Llibre</h6>
+       <div v-for="j in 1" :key="j">
+          <h5>AUTOR</h5>
+       </div>
+       <h6>Valoració</h6>
+       <h6>PREU</h6>
+       <b-button variant="danger">Add to cart</b-button>
+     </div>
+   </div>
+ </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
