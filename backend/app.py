dca7b29a82a1ff033f90c6153937ac8c1502ebe1
ferranSanchezUB <fsanchll43@alumnes.ub.edu>
2020-10-23 13:52:21 +0200

    Implementacio del model de llibre

--- backend/app.py
+++ backend/app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_cors import CORS
-from flask import render_template
-import os
+from flask_migrate import Migrate
+
+from db import db, secret_key
+from model.books import BooksModel
 
 app = Flask(__name__, static_folder="/home/pau/Escritorio/Grup-ES/frontend/dist/static",
          template_folder="/home/pau/Escritorio/Grup-ES/frontend/dist")
 
 CORS(app, resources={r'/*': {'origins': '*'}})
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = secret_key
+
+migrate = Migrate(app, db)
+db.init_app(app)
 
 @app.route('/')
 def render():
