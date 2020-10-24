4d256781d05c9e12dc3a027328d198dd79841467
aaronPeruga <perugaaron99@gmail.com>
2020-10-24 16:52:34 +0200

    frontend add book

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import AddBooks from '@/components/AddBooks.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'add_books',
+      component: AddBooks
     }
   ]
 })
