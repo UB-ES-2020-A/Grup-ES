f741b185e7804c41ae2772e8bfb5ad31b8e28e63
aaronPeruga <perugaaron99@gmail.com>
2020-10-25 15:02:04 +0100

    frontend delte book

---
+++ frontend/src/components/DeleteBooks.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <b-navbar toggleable="lg" type="dark" variant="info">
+      <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    </b-navbar>
+    <b-container>
+      <div class="row d-flex justify-content-center">
+      <div class="col-sm-4">
+      <div class="form-control bg-light" style="margin-top: 100px">
+      <div class="form-label-group">
+              <div class="row justify-content-center"><h4>Borrar libro</h4></div>
+              <b-row>
+              <b-container>
+              <b-row align-h="between">
+                <b-col>
+                  <input id="isbn" class="form-control"
+                  placeholder="ISBN" required autofocus v-model="isbn" style="margin-top: 15px">
+                </b-col>
+              </b-row>
+              <b-button v-b-modal.modal-1 variant="primary" style="margin-top: 10px">Borrar libro</b-button>
+             </b-container>
+             </b-row>
+
+      </div>
+      </div>
+      </div>
+
+      <!-- modal -->
+      <b-modal id="modal-1" hide-footer title="BootstrapVue">
+        <p class="my-4">¿Estas seguro de borrar el libro: XXXXXX?</p>
+        <b-row align-h="center">
+          <b-col cols="2">
+            <b-button @click="$bvModal.hide('modal-1')"> OK </b-button>
+          </b-col>
+          <b-col cols="2">
+            <b-button @click="$bvModal.hide('modal-1')"> NO </b-button>
+          </b-col>
+        </b-row>
+      </b-modal>
+
+      <div class="col-sm-6">
+      <b-container style="margin-top: 100px">
+        <img :src="'https://placehold.it/240x340/'" >
+      </b-container>
+      </div>
+
+      </div>
+    </b-container>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  methods: {
+  }
+}
+</script>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import DeleteBooks from '@/components/DeleteBooks.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'delete_books',
+      component: DeleteBooks
     }
   ]
 })
