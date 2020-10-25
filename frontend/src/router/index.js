7b79f9118f054d007ec3dd7002f943532a9cf800
aaronPeruga <perugaaron99@gmail.com>
2020-10-25 13:22:27 +0100

    frontend modify book

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import ModifyBooks from '@/components/ModifyBooks.vue'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'modify_books',
+      component: ModifyBooks
     }
   ]
 })
