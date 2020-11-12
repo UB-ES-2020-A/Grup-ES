83183026136db55b60d252765547bc886a981117
paubernabe <pbernaco7@alumnes.ub.edu>
2020-11-12 13:18:19 +0100

    correccio # a la url

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -11,6 +11,8 @@ import RegisterForm from '@/components/RegisterForm.vue'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
