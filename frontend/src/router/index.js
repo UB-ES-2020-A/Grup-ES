b1d46bdf551f232f6d37eed3b30fd3ad356f8756
Guillem Molina <gmolinga32@alumnes.ub.edu>
2020-11-14 17:32:26 +0100

    Added MisPedidos basic structure

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,6 +7,7 @@ import ModifyBooks from '@/components/ModifyBooks.vue'
 import Login from '@/components/Login.vue'
 import AddBooks from '@/components/AddBooks.vue'
 import RegisterForm from '@/components/RegisterForm.vue'
+import MisPedidos from '@/components/MisPedidos.vue'
 
 Vue.use(Router)
 
@@ -47,6 +48,11 @@ export default new Router({
       path: '/add',
       name: 'AddBooks',
       component: AddBooks
+    },
+    {
+      path: '/mispedidos',
+      name: 'MisPedidos',
+      component: MisPedidos
     }
   ]
 })
