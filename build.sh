4521d42b14eda0b6555225943f1b8a110de612b7
aaronPeruga <perugaaron99@gmail.com>
2020-11-15 22:55:43 +0100

    fix deploy

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ cp frontend/dist/index.html -r deployment/templates
 cp frontend/dist/static -r deployment
 cp backend/resources -r deployment
 cp backend/model -r deployment
+cp backend/utils -r deployment
 cp backend/*.py deployment
