6a1e7694b27a8ce72555e6ebb18330f38020dcbb
Guillem Molina <gmolinga32@alumnes.ub.edu>
2020-11-20 20:14:18 +0100

    Tests post endpoint

--- backend/tests/test_REV_3.py
+++ backend/tests/test_REV_3.py
@@ -1,3 +1,5 @@
+import base64
+import json
 import unittest
 
 from tests.base_test import BaseTest
@@ -17,6 +19,11 @@ class UnitTestOfUS(BaseTest):
         user = UsersModel("username", "email@gmail.com")
         user.hash_password("p4ssw0rd!")
         return user
+
+    @classmethod
+    def login_dummy_user(cls, client):
+        res = client.post("/login", data={"email": "email@gmail.com", "password": "p4ssw0rd!"})
+        return json.loads(res.data)["token"]
 
     # TEST TASK 2
     def test_model_add_review(self):
@@ -136,6 +143,111 @@ class UnitTestOfUS(BaseTest):
             ScoresModel.remove_review(review)
             with self.assertRaises(Exception):
                 ScoresModel.remove_review(review)
+
+    # TEST TASK 3
+    def test_post_review(self):
+        with self.app.app_context():
+            book = self.dummy_book()
+            book.save_to_db()
+
+            user = self.dummy_user()
+            user.save_to_db()
+
+            token = self.login_dummy_user(self.client)
+
+            parameters = {
+                'isbn': book.isbn,
+                'email': user.email,
+                'score': 5,
+                'review': "This book is good!"
+            }
+
+            res = self.client.post("/review", data=parameters, headers={
+                "Authorization": 'Basic ' + base64.b64encode((token + ":").encode('ascii')).decode('ascii')
+            })
+            self.assertEqual(201, res.status_code)
+            json.loads(res.data)
+            self.assertEqual(ReviewsModel.find_by_isbn_user_id(book.isbn, user.id).json(), json.loads(res.data))
+
+    def test_post_review_without_login(self):
+        with self.app.app_context():
+            user = self.dummy_user()
+            user.save_to_db()
+
+            book = self.dummy_book()
+            book.save_to_db()
+
+            res = self.client.post("/review")
+            self.assertEqual(401, res.status_code)
+
+    def test_post_review_non_existing_book(self):
+        with self.app.app_context():
+            user = self.dummy_user()
+            user.save_to_db()
+
+            token = self.login_dummy_user(self.client)
+
+            parameters = {
+                'isbn': 1,
+                'email': user.email,
+                'score': 5,
+                'review': "This is a extensive review of how this book is so good!"
+            }
+
+            res = self.client.post("/review", data=parameters, headers={
+                "Authorization": 'Basic ' + base64.b64encode((token + ":").encode('ascii')).decode('ascii')
+            })
+            self.assertEqual(404, res.status_code)
+
+    def test_post_two_reviews_same_book(self):
+        with self.app.app_context():
+            book = self.dummy_book()
+            book.save_to_db()
+
+            user = self.dummy_user()
+            user.save_to_db()
+
+            token = self.login_dummy_user(self.client)
+
+            parameters = {
+                'isbn': book.isbn,
+                'email': user.email,
+                'score': 5,
+                'review': "This is a extensive review of how this book is so good!"
+            }
+
+            res = self.client.post("/review", data=parameters, headers={
+                "Authorization": 'Basic ' + base64.b64encode((token + ":").encode('ascii')).decode('ascii')
+            })
+            res = self.client.post("/review", data=parameters, headers={
+                "Authorization": 'Basic ' + base64.b64encode((token + ":").encode('ascii')).decode('ascii')
+            })
+            self.assertEqual(500, res.status_code)
+
+
+def test_post_review_wrong_score(self):
+    with self.app.app_context():
+        book = self.dummy_book()
+        book.save_to_db()
+
+        user = self.dummy_user()
+        user.save_to_db()
+
+        token = self.login_dummy_user(self.client)
+
+        parameters = {
+            'isbn': book.isbn,
+            'email': user.email,
+            'score': 0,
+            'review': "Wow such book should not exist"
+        }
+
+        res = self.client.post("/review", data=parameters, headers={
+            "Authorization": 'Basic ' + base64.b64encode((token + ":").encode('ascii')).decode('ascii')
+        })
+        self.assertEqual(418, res.status_code)
+
+
 
 
 if __name__ == '__main__':
