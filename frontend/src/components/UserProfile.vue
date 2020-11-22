eaab6305c24ecece3022010d9ca5dfbfe20a40e4
paubernabe <pbernaco7@alumnes.ub.edu>
2020-11-22 20:08:40 +0100

    modal settings user profile

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -2,12 +2,13 @@
 <div id="app">
 <navbar @changeShowState="show = !show"/>
 <div class="body" v-if="show === true">
+  <settings :user="user"/>
   <b-container>
     <b-row>
-    <b-col sm="12" md="6" lg="4" xl="4" style ="background-color: #e9ecef">
+    <b-col sm="12" md="6" lg="4" xl="4" offset-sm="0" offset-md="1" offset-lg="1" offset-xl="1">
       <b-row class="d-flex justify-content-center">
-        <b-icon icon="person-circle" font-scale="4"></b-icon>
-        <b-icon icon="pencil-fill" font-scale="1"></b-icon>
+        <b-icon icon="person-circle" font-scale="4" class="m-3"></b-icon>
+        <b-icon icon="pencil-fill" v-b-modal.settings font-scale="1"></b-icon>
       </b-row>
       <br>
       <b-row class="d-flex justify-content-center">
@@ -19,14 +20,43 @@
         <h4> {{user.email}} </h4>
       </b-row>
     </b-col>
-    <b-col sm="12" md="6" lg="8" xl="8">
+
+    <b-col sm="12" md="5" lg="7" xl="7">
       <b-row>
         <h3> La teva biblioteca </h3>
       </b-row>
       <b-row>
-
+        <b-col  v-for="(lib) in library" :key="lib.book.isbn">
+        <br>
+        <img :src="getURL(lib)" style="height:209px; width:140px;" alt="">
+        <h6  @click = "gotobook(lib.book.isbn)">{{ lib.book.titulo }}</h6>
+        <h5>{{ lib.book.autor }}</h5>
+        </b-col>
       </b-row>
-
+      <br>
+      <br>
+      <b-row>
+        <h3> Les teves últimes compres </h3>
+      </b-row>
+      <b-row>
+        <b-col  v-for="(ped) in pedidos" :key="ped.book.isbn">
+        <br>
+        <img :src="getURL(lib)" style="height:209px; width:140px;" alt="">
+        <h6  @click = "gotobook(ped.book.isbn)">{{ ped.book.titulo }}</h6>
+        <h5>{{ ped.book.autor }}</h5>
+        </b-col>
+      </b-row>
+      <b-row>
+        <h3> També et podrien interessar... </h3>
+      </b-row>
+      <b-row>
+        <b-col  v-for="(ped) in pedidos" :key="ped.book.isbn">
+        <br>
+        <img :src="getURL(lib)" style="height:209px; width:140px;" alt="">
+        <h6  @click = "gotobook(ped.book.isbn)">{{ ped.book.titulo }}</h6>
+        <h5>{{ ped.book.autor }}</h5>
+        </b-col>
+      </b-row>
     </b-col>
     </b-row>
   </b-container>
@@ -39,22 +69,29 @@
 </template>
 
 <script>
+import axios from 'axios'
 import navbar from './subcomponents/navbar'
 import foot from './subcomponents/foot'
+import settings from './subcomponents/ProfileSettings'
 
 export default {
   components: {
     navbar,
-    foot
+    foot,
+    settings
   },
   data () {
     return {
       show: true,
-      user: {}
+      user: {},
+      pedidos: [],
+      library: []
     }
   },
   created () {
     this.fetch_cache()
+    this.load_library()
+    this.load_pedidos()
   },
   methods: {
     fetch_cache () {
@@ -65,6 +102,32 @@ export default {
         this.session_status = 'Log Out'
         this.session_boolean = true
       }
+    },
+    load_pedidos () {
+      const path = 'https://grup-es.herokuapp.com/transactions/' + this.user.email
+      axios.get(path, { auth: { username: this.user.token } })
+        .then((res) => {
+          this.pedidos = res.data.transactions
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    },
+    load_library () {
+      const path = 'https://grup-es.herokuapp.com/library/' + this.user.email
+      axios.get(path, {
+        auth: {username: this.user.token}
+      })
+        .then((res) => {
+          this.library = res.data.library
+          console.log(this.library)
+        })
+        .catch((error) => {
+          console.error(error)
+        })
+    },
+    getURL (lib) {
+      return lib.book.url_imagen
     }
   }
 }
---
+++ frontend/src/components/subcomponents/ProfileSettings.vue
@@ -0,0 +1,132 @@
+<template>
+  <div>
+    <b-modal
+    id="settings"
+    title="Configuració del perfil"
+    size="xl"
+    hide-footer="true"
+    >
+    <b-tabs content-class="mt-3">
+      <b-tab title="Usuari" active>
+       <form ref="form" @submit.stop.prevent="handleSubmit">
+       <b-row>
+       <b-col>
+        <b-form-group
+          label="Username"
+          label-for="username-input"
+          invalid-feedback="usernameInput"
+        >
+        <b-form-input
+          id="username-input"
+          v-model="this.$props.user.username"
+          type="text"
+        ></b-form-input>
+      </b-form-group>
+      <b-button size="md" variant="danger">Canviar nom d'usuari</b-button>
+      </b-col>
+      </b-row>
+      </form>
+      </b-tab>
+      <b-tab title="Credencials">
+      <form ref="form" @submit.stop.prevent="handleSubmit">
+      <b-row>
+      <b-col>
+       <b-form-group
+         label="Introdueix l'actual contrasenya"
+         label-for="pwdnow-input"
+         invalid-feedback="pwdInput"
+       >
+       <b-form-input
+         id="pwdnow-input"
+         v-model="this.$props.user.username"
+         type="password"
+       ></b-form-input>
+     </b-form-group>
+     <b-form-group
+       label="Introdueix la nova contrasenya"
+       label-for="pwdnew1-input"
+       invalid-feedback="pwdInput"
+     >
+     <b-form-input
+       id="pwdnew1-input"
+       v-model="this.$props.user.username"
+       type="password"
+     ></b-form-input>
+   </b-form-group>
+   <b-form-group
+     label="Repeteix la nova contrasenya"
+     label-for="pwdnew2-input"
+     invalid-feedback="pwdInput"
+   >
+   <b-form-input
+     id="pwdnew2-input"
+     v-model="this.$props.user.username"
+     type="password"
+   ></b-form-input>
+   </b-form-group>
+     <b-button size="md" variant="danger">Canviar contrasenya</b-button>
+     </b-col>
+     </b-row>
+     </form>
+      </b-tab>
+      <b-tab title="Configuració avançada">
+      <form ref="form" @submit.stop.prevent="handleSubmit">
+      <b-row>
+      <b-col>
+       <b-form-group
+         label="Introdueix el teu correu electrònic"
+         label-for="mail-input"
+         invalid-feedback="pwdInput"
+       >
+       <b-form-input
+         id="mail-input"
+         v-model="this.$props.user.email"
+         type="email"
+       ></b-form-input>
+     </b-form-group>
+     <b-form-group
+       label="Contrasenya"
+       label-for="pwd-input"
+       invalid-feedback="pwdInput"
+     >
+     <b-form-input
+       id="pwd-input"
+       type="password"
+     ></b-form-input>
+   </b-form-group>
+   <b-form-group
+     label="Repeteix la contrasenya"
+     label-for="pwd2-input"
+     invalid-feedback="pwdInput"
+   >
+   <b-form-input
+     id="pwd2-input"
+     type="password"
+   ></b-form-input>
+   </b-form-group>
+     <b-button size="md" variant="danger">Desactivar compte</b-button>
+     </b-col>
+     </b-row>
+     </form>
+      </b-tab>
+    </b-tabs>
+    </b-modal>
+  </div>
+</template>
+
+<script>
+
+export default {
+  props: {
+    user: Object
+  },
+  data () {
+    return {
+      username: '',
+      mail1: ''
+    }
+  },
+  methods: {
+  }
+}
+</script>
