8fd5f6199dc5636bd87d186c350799cf381455d9
Francina <frponsll40@alumnes.ub.edu>
2020-12-07 16:46:13 +0100

    Filtrar llibres només per vendibles.
    sempre que s'utilitzi l'endpoint BooksList, només es tenen en compte els vendibles.

--- backend/resources/books.py
+++ backend/resources/books.py
@@ -124,14 +124,15 @@ class BooksList(Resource):
         parser.add_argument('score', type=bool, required=False,
                             help="Indicates if returning the score of the book is needed .")
         data = parser.parse_args()
+        books = BooksModel.query.filter_by(vendible=True)
         with lock:
             if data['param'] is None:
-                books = BooksModel.query.limit(data['numBooks']).all()
+                books = books.limit(data['numBooks']).all()
             else:
                 if data['order'] == "asc":
-                    books = BooksModel.query.order_by(asc(data['param'])).limit(data['numBooks']).all()
+                    books = books.order_by(asc(data['param'])).limit(data['numBooks']).all()
                 else:
-                    books = BooksModel.query.order_by(desc(data['param'])).limit(data['numBooks']).all()
+                    books = books.order_by(desc(data['param'])).limit(data['numBooks']).all()
         return {'books': [book.json(reviews=data['reviews'], score=data['score']) for book in books]}, 200
 
 
