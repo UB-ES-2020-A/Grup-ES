b435c6480fb282e1199a6f0dbb5903a1442f17a1
ferranSanchezUB <fsanchll43@alumnes.ub.edu>
2020-12-10 23:25:40 +0100

    Afegit parametre 'showOnlyVendibles' + Correcio BUG parametres bool. - Afegit el camp 'showOnlyVendibles' que per defecte es True, que indicara si la query ha de filtra els llibres que estan vendibles. Aplicat en els endpoints de /books i /trending. - Correcio dels camps booleans de tots els endpoints que detectaven el False els parametres False com True.

--- backend/tests/test_ESCP_4.py
+++ backend/tests/test_ESCP_4.py
@@ -8,13 +8,16 @@ from tests.base_test import BaseTest
 
 class UnitTestOfUS(BaseTest):
 
+    def init_books(self):
+        self.book = BooksModel(1, 1, 1, "test")
+        self.book.save_to_db()
+        self.book2 = BooksModel(2, 1, 1, "as")
+        self.book2.save_to_db()
+
     # TEST TASK 1
     def test_get_book1(self):
         with self.app.app_context():
-            book = BooksModel(1, 1, 1, "test")
-            book.save_to_db()
-            book = BooksModel(2, 1, 1, "as")
-            book.save_to_db()
+            self.init_books()
             args = {
                 "numBooks": 2,
                 "param": "isbn",
@@ -27,10 +30,7 @@ class UnitTestOfUS(BaseTest):
 
     def test_get_books(self):
         with self.app.app_context():
-            book = BooksModel(1, 1, 1, "test")
-            book.save_to_db()
-            book = BooksModel(2, 1, 1, "as")
-            book.save_to_db()
+            self.init_books()
             res = self.client.get('/api/books')
             self.assertEqual(200, res.status_code)
             list_books = list(map(lambda u: u.json(), BooksModel.query.all()))
@@ -38,11 +38,9 @@ class UnitTestOfUS(BaseTest):
 
     def test_get_book2(self):
         with self.app.app_context():
-            book1 = BooksModel(1, 1, 1, "aaaa")
-            book1.save_to_db()
-            book2 = BooksModel(2, 1, 1, "bbbb")
-            book2.save_to_db()
-            book2.vendible = False
+            self.init_books()
+            self.book2.update_from_db({"vendible": False})
+
             args = {
                 "numBooks": 2,
                 "param": "titulo",
@@ -53,12 +51,36 @@ class UnitTestOfUS(BaseTest):
             list_books = list(map(lambda u: u.json(), BooksModel.query.filter_by(vendible=True).order_by(asc('titulo')).limit(2).all()))
             self.assertEqual(list_books, json.loads(res.data)["books"])
 
+    def test_get_book3(self):
+        with self.app.app_context():
+            self.init_books()
+            self.book2.update_from_db({"vendible": False})
+
+            args = {
+                "showOnlyVendibles": True
+            }
+            res = self.client.get('/api/books', data=args)
+            self.assertEqual(200, res.status_code)
+            list_books = list(map(lambda u: u.json(), BooksModel.query.filter_by(vendible=True).all()))
+            self.assertEqual(list_books, json.loads(res.data)["books"])
+
+    def test_get_books_all(self):
+        with self.app.app_context():
+            self.init_books()
+            self.book2.update_from_db({"vendible": False})
+
+            args = {
+                "showOnlyVendibles": False
+            }
+            res = self.client.get('/api/books', data=args)
+            self.assertEqual(200, res.status_code)
+            list_books = list(map(lambda u: u.json(), BooksModel.query.all()))
+            self.assertEqual(list_books, json.loads(res.data)["books"])
+
     def test_get_new_releases(self):
         with self.app.app_context():
-            book1 = BooksModel(1, 1, 1, "aaaa")
-            book1.save_to_db()
-            book2 = BooksModel(2, 1, 1, "bbbb")
-            book2.save_to_db()
+            self.init_books()
+
             args = {
                 "param": "fecha_de_publicacion",
                 "order": "desc"
@@ -70,11 +92,9 @@ class UnitTestOfUS(BaseTest):
 
     def test_get_new_releases_vendible_false(self):
         with self.app.app_context():
-            book1 = BooksModel(1, 1, 1, "aaaa")
-            book1.save_to_db()
-            book2 = BooksModel(2, 1, 1, "bbbb")
-            book2.save_to_db()
-            book2.vendible = False
+            self.init_books()
+            self.book2.update_from_db({"vendible": False})
+
             args = {
                 "param": "fecha_de_publicacion",
                 "order": "desc"
@@ -89,16 +109,15 @@ class UnitTestOfUS(BaseTest):
     # TEST TASK 2
     def test_get_book(self):
         with self.app.app_context():
-            book = BooksModel(1, 1, 1, "test")
-            book.save_to_db()
+            self.init_books()
 
-            res = self.client.get("/api/book/1")
+            res = self.client.get(f"/api/book/{self.book.isbn}")
             self.assertEqual(200, res.status_code)
-            self.assertEqual(book.json(), json.loads(res.data)["book"])
+            self.assertEqual(self.book.json(), json.loads(res.data)["book"])
 
     def test_get_invalid_book(self):
         with self.app.app_context():
-            res = self.client.get("/api/book/1")
+            res = self.client.get("/api/book/0")
             self.assertEqual(404, res.status_code)
 
 
